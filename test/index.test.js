c8877d9bdc1830a2200153f3eeb8a91331992531
Kiril Vatev <vatev.1@gmail.com>
2020-01-18 01:11:28 -0500

    adding png test, unknown format test, making sure errors reject the promise rather than throwing

--- test/index.test.js
+++ test/index.test.js
@@ -37,15 +37,36 @@ describe('heic-convert', () => {
     await assertImage(output100, 'image/jpeg', 'f7f1ae16c3fbf035d1b71b1995230305125236d0c9f0513c905ab1cb39fc68e9');
   });
 
-  it('throws if data other than a HEIC image is passed in', async () => {
+  it('converts known image to pmg', async () => {
+    const buffer = await readFile(path.resolve(root, 'temp', '0002.heic'));
+
+    const output = await convert({ buffer, format: 'PNG' });
+    await assertImage(output, 'image/png', '0efc9a4c58d053fb42591acd83f8a5005ee2844555af29b5aba77a766b317935');
+  });
+
+  it('errors for an unknown output format', async () => {
     const buffer = Buffer.from(Math.random().toString() + Math.random().toString());
 
-    try {
-      await convert({ buffer, format: 'JPEG' });
-      throw new Error('decoding succeeded when it was expected to fail');
-    } catch (e) {
-      expect(e).to.be.instanceof(TypeError)
-        .and.to.have.property('message', 'input buffer is not a HEIC image');
-    }
+    let error;
+
+    await convert({ buffer, format: 'PINEAPPLES' }).catch(err => {
+      error = err;
+    });
+
+    expect(error).to.be.instanceof(Error)
+      .and.to.have.property('message', 'output format needs to be one of [JPEG,PNG]');
+  });
+
+  it('errors if data other than a HEIC image is passed in', async () => {
+    const buffer = Buffer.from(Math.random().toString() + Math.random().toString());
+
+    let error;
+
+    await convert({ buffer, format: 'JPEG' }).catch(err => {
+      error = err;
+    });
+
+    expect(error).to.be.instanceof(TypeError)
+      .and.to.have.property('message', 'input buffer is not a HEIC image');
   });
 });
