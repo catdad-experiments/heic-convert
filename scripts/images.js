a02f68a51b84a0470e04cc083c813875ecee8af3
Kiril Vatev <vatev.1@gmail.com>
2020-08-19 17:57:49 -0400

    adding new control images

--- scripts/images.js
+++ scripts/images.js
@@ -14,8 +14,17 @@ const images = [{
   name: '0002.heic', // single image
   url: drive('1J_761fe_HWSijAthq7h_D2Zsf1_es1cT')
 }, {
+  name: '0002-control.png', // single image control
+  url: drive('1uomSNTAK5FifvI72lYi6T42zhvVv1LwH')
+}, {
   name: '0003.heic', // multiple images
   url: drive('1Iru2g084yZGz-eRagiqZgccRJZfGLYgS')
+}, {
+  name: '0003-0-control.png', // multiple images control, index 0
+  url: drive('1FZ9mJVj54MpD4c5TMfiOrarzkrT3pr2U')
+}, {
+  name: '0003-1-control.png', // multiple images control, index 1, 2
+  url: drive('1qD4-V6FcU2ffpNm0ZxKO2cpqbol73tok')
 }].map(img => {
   img.path = resolve(img.name);
   return img;
@@ -27,7 +36,7 @@ const images = [{
     const res = await fetch(url);
 
     if (!res.ok) {
-      throw new Error(`failed to download ${name} at ${url}`);
+      throw new Error(`failed to download ${name} at ${url}: ${res.status} ${res.statusText}`);
     }
 
     const buffer = await res.buffer();
