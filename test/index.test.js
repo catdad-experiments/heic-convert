4bc385162a667cf21c9281f23e37943cdbb8c793
Kiril Vatev <vatev.1@gmail.com>
2023-11-28 02:56:25 -0500

    just for funsies, adding legacy libheif js tests, for overkill backwards compatibility

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,22 @@
 const runTests = require('./run-tests.js');
 
-describe('heic-convert (default wasm)', () => {
+describe('heic-convert (default wasm libheif)', () => {
   runTests(require('..'));
+});
+
+// I wouldn't say these are strictly required, but 🤷‍♀️
+describe('heic-convert (legacy js libheif)', () => {
+  const libheif = require('libheif-js/index.js');
+  const decodeLib = require('heic-decode/lib.js');
+  const formats = require('../formats-node.js');
+
+  const { one: decodeOne, all: decodeAll } = decodeLib(libheif);
+  decodeOne.all = decodeAll;
+
+  const { one, all } = require('../lib.js')(decodeOne, formats);
+
+  const convert = one;
+  convert.all = all;
+
+  runTests(convert);
 });
